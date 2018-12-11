e5afffc1a6d34a452ef8a4824c73743a1a7b0b81
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-12-11 00:26:38 -0500

    [backport MR-4983] BUG: vtkExternalModuleMacros: Ensure all required VTK variables are set
    This commit is done anticipating the integration of support for enabling
    python wrapping of VTK module externally built.

--- CMake/vtkDirectories.cmake
+++ CMake/vtkDirectories.cmake
@@ -1,5 +1,7 @@
 include(GNUInstallDirs)
 
+include(vtkSupportMacros)
+
 # VTK installation structure
 set(vtk_subdir "vtk-${VTK_MAJOR_VERSION}.${VTK_MINOR_VERSION}")
 vtk_set_with_default(VTK_INSTALL_RUNTIME_DIR "${CMAKE_INSTALL_BINDIR}")
@@ -15,16 +17,21 @@ vtk_set_with_default(VTK_INSTALL_EXPORT_NAME "VTKTargets")
 vtk_set_with_default(VTK_INSTALL_NDK_MODULES_DIR "${VTK_INSTALL_DATA_DIR}/ndk-modules")
 
 # Set up our directory structure for output libraries and binaries
-vtk_set_with_default(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${VTK_BINARY_DIR}/bin")
+if(VTK_SOURCE_DIR)
+  set(output_dir ${VTK_BINARY_DIR})
+else()
+  set(output_dir ${CMAKE_BINARY_DIR})
+endif()
+vtk_set_with_default(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${output_dir}/bin")
 if(UNIX)
   set(vtk_library_directory "lib")
 else()
   set(vtk_library_directory "bin")
 endif()
-vtk_set_with_default(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${VTK_BINARY_DIR}/${vtk_library_directory}")
-vtk_set_with_default(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${VTK_BINARY_DIR}/lib")
-vtk_set_with_default(VTK_MODULES_DIR "${VTK_BINARY_DIR}/${VTK_INSTALL_PACKAGE_DIR}/Modules")
-vtk_set_with_default(VTK_WWW_DIR "${VTK_BINARY_DIR}/www")
+vtk_set_with_default(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${output_dir}/${vtk_library_directory}")
+vtk_set_with_default(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${output_dir}/lib")
+vtk_set_with_default(VTK_MODULES_DIR "${output_dir}/${VTK_INSTALL_PACKAGE_DIR}/Modules")
+vtk_set_with_default(VTK_WWW_DIR "${output_dir}/www")
 
 if(DEFINED VTK_INSTALL_PYTHON_MODULE_DIR)
   message(WARNING
